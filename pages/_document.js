b4510469c71a371e293ee6135d7b95d4f4b5ec88
Your Git Username <radu.p1995@yahoo.com>
2024-02-06 14:19:27 +0200

    asda

--- pages/_document.js
+++ pages/_document.js
@@ -6,11 +6,6 @@ class MyDocument extends Document {
       <Html>
         <Head>
           {/* You can add custom tags here, but do not remove the <Head> component */}
-          <title>Cristina Zurba</title>
-          <meta
-            name="description"
-            content="Cristina Zurba is an interactive tarot card game website developed by well-known youtuber, tarot reader and astrologer Cristina Zurba."
-          />
         </Head>
         <body>
           <Main />
