c1f8c1ee748663698f79480feb6998c30988abf7
Your Git Username <radu.p1995@yahoo.com>
2024-02-07 08:46:30 +0200

    new add

--- pages/citire-viitor/index.jsx
+++ pages/citire-viitor/index.jsx
@@ -357,10 +357,6 @@ export function CitirePersonalizata({ services }) {
           content="https://cristinazurba.com/images/social-share.jpg"
         />
         <meta name="format-detection" content="telephone=no" />
-        <meta
-          name="google-adsense-account"
-          content="ca-pub-9577714849380446"
-        ></meta>
       </Head>
       <div
         style={{
