a0d9424f5228892b915147b1ebe70e3b461c4f42
Your Git Username <radu.p1995@yahoo.com>
2024-02-26 19:44:35 +0200

    asdasda

--- pages/_document.js
+++ pages/_document.js
@@ -5,7 +5,6 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          {/* Pastreaza meta tag-ul Google AdSense aici */}
           {/* You can add custom tags here, but do not remove the <Head> component */}
           <meta
             name="google-adsense-account"
@@ -21,24 +20,6 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 })(window,document,'script','dataLayer','GTM-P2LWMRCF');`,
             }}
           />
-
-          {/* Google Tag Manager - Global site tag (gtag.js) */}
-          <script
-            async
-            src="https://www.googletagmanager.com/gtag/js?id=AW-11367210761"
-          ></script>
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `
-                window.dataLayer = window.dataLayer || [];
-                function gtag(){dataLayer.push(arguments);}
-                gtag('js', new Date());
-                gtag('config', 'AW-11367210761');
-              `,
-            }}
-          />
-
-          {/* Aici poti adauga alte scripturi globale necesare */}
         </Head>
         <body>
           <noscript>
@@ -51,21 +32,6 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
           </noscript>
           <Main />
           <NextScript />
-          {/* Adaugarea scripturilor Google AdWords in body */}
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `
-                document.addEventListener('click', function(e) {
-                  if (e.target.closest('button') && e.target.closest('button').innerText.includes("LOGIN")) {
-                    gtag('event', 'conversion', {
-                      'send_to': 'AW-11367210761/RBplCOHhkpYZEIm-p6wq'
-                    });
-                  }
-                });
-                gtag('event', 'conversion', {'send_to': 'AW-11367210761/oIR1COThkpYZEIm-p6wq'});
-              `,
-            }}
-          />
         </body>
       </Html>
     );
