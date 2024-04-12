4213f13e82b29867b9ef754fd96bd88fe2a1665a
Adrian <radu.p1995@yahoo.com>
2024-04-12 09:31:00 +0300

    test here...

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -3,7 +3,7 @@ const path = require("path");
 module.exports = {
   i18n: {
     localePath: path.resolve("./public/locales"),
-    defaultLocale: "en",
+    defaultLocale: "ro",
     locales: [
       "en", // English
       "ro", // Romanian
