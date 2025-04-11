1b357bf0f7a2bcf633c4a85c9ca3c5daf55d22d2
Adrian <radu.p1995@yahoo.com>
2025-04-11 12:10:07 +0300

    reparat problema facturi

--- pages/api/download-archive.js
+++ pages/api/download-archive.js
@@ -37,9 +37,11 @@ export default async function handler(req, res) {
           const { pdfUrl } = await pdfResponse.json();
 
           if (pdfUrl) {
-            // Fetch pentru a obține conținutul PDF de la link-ul furnizat de Stripe
-            const pdfBuffer = await fetch(pdfUrl).then((res) => res.buffer());
-
+// Preia conținutul PDF de la link-ul furnizat de Stripe
+const pdfResponse = await fetch(pdfUrl);
+const arrayBuffer = await pdfResponse.arrayBuffer();
+const pdfBuffer = Buffer.from(arrayBuffer);
+            
             // Aici adăugăm logica pentru a include ziua, luna și anul în numele fișierului PDF
             const invoiceDetailsResponse = await fetch(
               `https://${url}/api/get-invoice-details?invoiceId=${invoiceId}`
