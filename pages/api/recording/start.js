51356f009a385124cd8e225d958f75cff5fbaea6
Code-W-A <radu.p1995@yahoo.com>
2025-08-03 17:30:28 +0300

    asdas

--- pages/api/recording/start.js
+++ pages/api/recording/start.js
@@ -174,6 +174,9 @@ export default async function handler(req, res) {
   const requestStartTime = Date.now();
   
   console.log('🎬 [AGORA START] === API CALLED ===');
+  console.log('🎬 [AGORA START] === API CALLED ===');
+  console.log('🎬 [AGORA START] === API CALLED ===');
+  console.log('🎬 [AGORA START] === API CALLED ===');
   console.log('🎬 [AGORA START] Method:', req.method);
   console.log('🎬 [AGORA START] Body:', JSON.stringify(req.body, null, 2));
   console.log('🎬 [AGORA START] Headers:', JSON.stringify(req.headers, null, 2));
